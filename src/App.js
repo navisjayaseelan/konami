6e6b089a32108dc3cc352bc1f3f475355d4af29c
navis.jayaseelan@healthicity.com <navis.jayaseelan@healthicity.com>
2019-01-15 15:27:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Konami from './components/Konami'
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      activated: false,
+      name: ''
+    };
+    this.toggleActivation = this.toggleActivation.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleActivation(){
+    if (this.state.activated === true) {
+      this.setState({activated: false});
+    } else {
+      this.setState({activated: true});
+    }
+  }
+
+  handleChange(e) {
+    this.setState({name: e.target.value });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,14 +35,13 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <h1>Welcome{this.state.name.length > 1 ? `, ${this.state.name}` : ``}</h1>
+
+          <input type="text" onChange={this.handleChange} placeholder="Your name" />
+
+          <button onClick={this.toggleActivation}>Toggle Konami Code!</button>
+          {this.state.activated ? <Konami name={this.state.name}/> : <div>Konami Code: Deactivated</div>}
         </header>
       </div>
     );
---
+++ src/components/Konami.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Mousetrap from 'mousetrap';
+
+class Konami extends Component {
+
+  constructor(props) {
+    super(props);
+    this.popUp = this.popUp.bind(this);
+  }
+
+  componentDidMount() {
+    Mousetrap.bind([`1 2 3 4`,`up up down down left right left right b a`], this.popUp);
+  }
+
+  componentWillUnmount() {
+    Mousetrap.unbind([`1 2 3 4`,`up up down down left right left right b a`]);
+  }
+
+  popUp() {
+    alert(`You inputted the Konami Code${this.props.name.length > 1 ? `, ${this.props.name}!` : `!`}`);
+  }
+
+  render() {
+    return (
+      <div>Hello {this.props.name} and this is Konami App.</div>
+    );
+  }
+}
+
+export default Konami;
